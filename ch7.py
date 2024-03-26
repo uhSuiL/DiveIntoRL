2c28938159e20eee3c307cda00181c10fc7c654c
Shu Liu <liushu_public@yeah.net>
2024-03-26 15:23:48 +0800

    add Qnet

--- ch7.py
+++ ch7.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 import torch as th
+from torch import nn
 from collections import deque
 
 
@@ -13,3 +14,21 @@ class ReplayBuffer(deque):
 	@property
 	def size(self):
 		return len(self)
+
+
+class Qnet:
+	def __init__(self, num_state: int, hidden: list, num_action: int, device: str):
+		self.num_state = num_state
+		self.hidden = hidden
+		self.num_action = num_action
+		self.device = device
+
+	def __call__(self, *args, **kwargs):
+		layers = [nn.Linear(self.num_state, self.hidden[0])]
+		for i in range(len(self.hidden) - 1):
+			layers.append(nn.Linear(self.hidden[i], self.hidden[i + 1]))
+		layers.append(nn.Linear(self.hidden[-1], self.num_action))
+
+		return nn.Sequential(*layers).to(self.device)
+
+
