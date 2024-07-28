982c01e694e2166e57c2d47fb994eeb2c2f8a063
Shu Liu <liushu_public@yeah.net>
2024-07-28 13:25:04 +0800

    add MLP

--- util.py
+++ util.py
@@ -1,3 +1,6 @@
+from torch import nn
+
+
 def lazy_init(cls):
 	class Tmp:
 		def __init__(self, *args, **kwargs):
@@ -8,3 +11,11 @@ def lazy_init(cls):
 			return cls(*self.args, **self.kwargs)
 
 	return Tmp
+
+
+def MLP(layers_dims: list[int]) -> nn.Module:
+	layers = []
+	for i in range(len(layers_dims) - 2):
+		layers += [nn.Linear(layers_dims[i], layers_dims[i+1]), nn.ReLU()]
+	layers.append(nn.Linear(layers_dims[-2], layers_dims[-1]))
+	return nn.Sequential(*layers)
